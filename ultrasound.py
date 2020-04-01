380f16c447036983d5c4f71ceb16360401c2f12f
Pi <pi>
2020-04-01 23:43:57 +0200

    class implemented

--- ultrasound.py
+++ ultrasound.py
@@ -4,52 +4,50 @@ import time
 
 gpio.setmode(gpio.BCM)
 
-GPIO_TRIGGER = 27
-GPIO_ECHO = 17
-# 23 echo, 24 trigger
-
-gpio.setup(GPIO_TRIGGER, gpio.OUT)
-gpio.setup(GPIO_ECHO, gpio.IN)
-
 class UltraSound(object):
     def __init__(self, trigger_pin, echo_pin, max_echo_wait_time=1.2 * 8/343.2):
         self.trigger_pin = trigger_pin
         self.echo_pin = echo_pin
         self.max_echo_wait_time = max_echo_wait_time
+        gpio.setup(self.trigger_pin, gpio.OUT)
+        gpio.setup(self.echo_pin, gpio.IN)
 
-def distance():
-    #print("Sending Pulse")
-    # Send 10us pulse
-    gpio.output(GPIO_TRIGGER, True)
-    time.sleep(10e-6)
-    gpio.output(GPIO_TRIGGER, False)
+    def get_distance(self):
+        # Send 10us pulse
+        gpio.output(self.trigger_pin, True)
+        time.sleep(10e-6)
+        gpio.output(self.trigger_pin, False)
 
-    StartTime = time.time()
-    StopTime = time.time()
+        pulse_start = time.time()
+        pulse_stop = time.time()
 
-    #print("Waiting for echo")
-    # TODO inf loop if no echo
-    # Push StartTime until pulse starts
-    while gpio.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-    # Push StopTime until pulse ends
-    while gpio.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+        # TODO inf loop if no echo
+        # Wait until pulse starts
+        while gpio.input(self.echo_pin) == 0:
+            pulse_start = time.time()
+        # Wait until pulse ends
+        while gpio.input(self.echo_pin) == 1:
+            pulse_stop = time.time()
 
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = 343.2 * TimeElapsed / 2
-
-    return distance
+        pulse_length = pulse_stop - pulse_start
+        # sonic speed is 343.2 m/s
+        # divide by 2, because forth and back
+        distance = 343.2 * pulse_length/ 2
+        return distance
 
 if __name__ == '__main__':
+    us1 = UltraSound(trigger_pin=27, echo_pin=17)
+    #us2 = UltraSound(trigger_pin=24, echo_pin=23)
+    #uss = [us1, us2]
+    uss = [us1]
+
     try:
         while True:
-            dist = distance()
-            print("%.2f m" % dist)
-            time.sleep(0.5)
+            for ind, us in enumerate(uss):
+                dist = us.get_distance()
+                print("US %d: %.2f m" % (ind,dist))
+                time.sleep(0.5)
     # Reset by pressing CTRL + C
     except KeyboardInterrupt:
-        print("Measurement stopped by User")
         gpio.cleanup()
+
